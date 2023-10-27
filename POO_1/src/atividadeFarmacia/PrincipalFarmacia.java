dc3dcc1b34c846b77b0e0e07a9aa2b749c589712
GuilhermeLopesCardoso <guilhermelopescardoso5@gmail.com>
2023-10-27 11:20:59 -0300

    Alterações atividadeFarmacia

--- POO_1/src/atividadeFarmacia/PrincipalFarmacia.java
+++ POO_1/src/atividadeFarmacia/PrincipalFarmacia.java
@@ -5,30 +5,26 @@ import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 public class PrincipalFarmacia {
+    public static ArrayList<Produto> produtos = new ArrayList<Produto>();
     public static void main(String[] args) {
-        ArrayList<Produto> produtos = new ArrayList<Produto>();
-
-        public static int menu() {
-            int menu = "1 - Cadastrar Produto \n" +
-                       "+99 - Sair";
-
-                    return Integer.parseInt(JOptionPane.showInputDialog(menu));
-        }
-
-
         int op = 0;
-
         do {
-            op = menu();
-            switch (op) {
-                case 1:
-                    Produto();
-                    break;
-            
-                    
-
-            }
-        }
-
+                op = menu();
+                switch (op) {
+                    case 1:
+                            Produto();
+                            break;
+                }
+        } while(op != 99);
     }
+    public static int menu() {
+        String menu = "1 - Cadastrar Produto\n"
+                    + "2 - Fazer Depósito \n"
+                    + "3 - Fazer um saque \n"
+                    + "4 - Fazer uma tranferencia \n"
+                    + "5 - Exibir Extrato \n"
+                    + "99 - Sair";
+            return Integer.parseInt(JOptionPane.showInputDialog(menu));
+    }
+
 }
